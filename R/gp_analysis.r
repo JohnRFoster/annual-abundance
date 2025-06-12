16af913ab53e1ad0955c0099049388a1a94c3c1d
JohnRFoster <fosterj@bu.edu>
2025-06-12 08:27:50 -0600

    add time data loading, enhance density plot labels, and adjust node variable names

--- R/gp_analysis.r
+++ R/gp_analysis.r
@@ -1,4 +1,5 @@
 samples <- read_rds("out/gp_samples.rds")
+time_join <- read_rds("out/gp_data.rds")
 
 unobserved <- which(is.na(time_join$pred_density))
 
@@ -7,14 +8,16 @@ node <- paste0("mu_x[", idx, "]")
 hist(samples[[node]], breaks = 50, main = paste0("Posterior of ", node))
 abline(v = time_join$pred_density[idx], col = "red", lwd = 2)
 
-mu_x <- samples[, grep("mu_x", colnames(samples_m))]
+mu_x <- samples[, grep("mu_x", colnames(samples))]
 med_dens <- apply(mu_x, 2, median)
+
+summary(apply(mu_x, 2, min))
 
 plot(
 	time_join$pred_density[-unobserved],
 	med_dens[-unobserved],
-	xlab = "Observed Density",
-	ylab = "Predicted Density",
+	xlab = "Observed (xgBoost) density",
+	ylab = "Predicted (GP) density",
 	main = "Posterior Median of Predicted Densities"
 )
 abline(0, 1, col = "red", lwd = 2)
@@ -22,5 +25,5 @@ abline(0, 1, col = "red", lwd = 2)
 cor(time_join$pred_density[-unobserved], med_dens[-unobserved])
 
 
-node <- "tau_cov"
+node <- "rho_t"
 hist(samples[[node]], breaks = 50, main = paste0("Posterior of ", node))
